0c117e03333643751a2e4cff071c6ac42c7a8488
Krish Vazirani <pacmengamings@gmail.com>
2024-11-14 14:11:40 -0500

    Update page.tsx

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,34 +1,19 @@
-"use server"
-import { redirectURL } from '@/lib/actions/urls';
-import { redirect } from 'next/navigation'
-import { visitCounter } from '@/lib/actions/urls';
-
+"use server";
+import { redirectURL, visitCounter } from '@/lib/actions/urls';
+import { redirect } from 'next/navigation';
 
 export default async function RedirectURLPage({
     params,
 }: {
-    params: { slug: string};
+    params: { slug: string; };
 }) {
-
-    const {slug} = await params;
+    const { slug } = params;
     const urlData = await redirectURL(slug);
 
-    if (urlData && urlData.originalUrl){
-        await visitCounter(urlData.urlId)
-        redirect(urlData.originalUrl)
-
-    }
-    else {
+    if (urlData && urlData.originalUrl) {
+        await visitCounter(urlData.urlId);
+        redirect(urlData.originalUrl);
+    } else {
         console.error("Error: Invalid URL or URL not found.");
     }
-
-
-
-    
-    
 }
-
-
-
-
-
