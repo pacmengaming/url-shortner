c9df85b995c3cb0393a72c62cab00ce14058e647
Krish Vazirani <pacmengamings@gmail.com>
2024-11-18 17:16:01 -0500

    slug param 2

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,16 +1,20 @@
 import { redirectURL, visitCounter } from '@/lib/actions/urls';
 import { notFound } from 'next/navigation';
 import { redirect } from 'next/navigation';
+import { useParams } from 'next/navigation';
 
-export default async function RedirectURLPage({ params }: { params: { slug: string } }) {
-    const slugValue = params.slug;
+export default async function RedirectURLPage() {
+  const { slug } = useParams();
 
-    const urlData = await redirectURL(slugValue);
+  if (!slug || typeof slug !== 'string') {
+    notFound();
+  }
+  const urlData = await redirectURL(slug);
 
-    if (urlData?.originalUrl) {
-        await visitCounter(urlData.urlId);
-        redirect(urlData.originalUrl);
-    } else {
-        notFound();
-    }
+  if (urlData?.originalUrl) {
+    await visitCounter(urlData.urlId);
+    redirect(urlData.originalUrl);
+  }
+
+  notFound();
 }
